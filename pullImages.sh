1177790325c3b4ee231adcfc7e02ebb5185dc3bd
masundi <masundi@nouveau-labs.com>
2020-10-04 11:44:42 +0530

    pullImages.sh file created to pull images from hub.docker.com

---
+++ pullImages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker image pull manju0707/sample-springboot-microservice-docker-cloud-config-server:0.0.1-SNAPSHOT 
+docker image pull manju0707/sample-springboot-microservice-docker-eureka-naming-server:0.0.1-SNAPSHOT 
+docker image pull manju0707/sample-springboot-microservice-docker-oauth2-authentication-server:0.0.1-SNAPSHOT 
+docker image pull manju0707/sample-springboot-microservice-docker-user-service:0.0.1-SNAPSHOT
+docker image pull manju0707/sample-springboot-microservice-docker-group-service:0.0.1-SNAPSHOT 
+docker image pull manju0707/sample-springboot-microservice-docker-zuul-api-gateway-server:0.0.1-SNAPSHOT 
