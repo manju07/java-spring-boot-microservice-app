dda87f2aba4cf4e01a8082393d22782a483bec5b
masundi <masundi@nouveau-labs.com>
2020-10-04 10:09:48 +0530

    eureka-naming-server added, this will manage all other services.

---
+++ eureka-naming-server/src/main/java/com/sample/springboot/microservices/eurekanamingserver/EurekaNamingServerApplication.java
@@ -0,0 +1,17 @@
+package com.sample.springboot.microservices.eurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * @author Manjunath Asundi
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ eureka-naming-server/src/test/java/com/sample/springboot/microservices/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sample.springboot.microservices.eurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * @author Manjunath Asundi
+ */
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
