6dace4a3beb67826795289fab504f3d2da89b6a0
masundi <masundi@nouveau-labs.com>
2020-10-04 10:08:32 +0530

    cloud-config-server added, this will fetch config files from github repository or local directory based on enabled environment.

---
+++ cloud-config-server/src/test/java/com/sample/springboot/microservices/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sample.springboot.microservices.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * @author Manjunath Asundi
+ */
+@SpringBootTest
+public class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
